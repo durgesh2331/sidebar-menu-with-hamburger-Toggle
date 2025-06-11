99a7f80eb43f67faf66acaeaad9e6a0d4eef5d37
Durgesh Prasad <42365460+durgesh2331@users.noreply.github.com>
2025-06-11 11:10:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let barsButton=document.getElementById("bar");
+let sidebar=document.getElementById("big-box-id");
+let closeBtn=document.getElementById("closed");
+
+barsButton.onclick=function(){
+  
+    sidebar.style.display = "block";
+   
+    
+}
+
+
+closeBtn.onclick=function(){
+  
+    sidebar.style.display = "none";
+   
+    
+}
